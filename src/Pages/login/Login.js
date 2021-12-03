f578c3a5764225ad7b93d39d8bd654a44b96d09e
Hamza-Mujahid <80825408+Hamza-Mujahid@users.noreply.github.com>
2021-12-03 18:32:12 +0500

    u

--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import Button from '../../components/button/Button'
 import ButtonSignUp from '../../components/button/ButtonSignUp'
-import LoginBox from '../../components/LoginBox/LoginBox'
+// import LoginBox from '../../components/LoginBox/LoginBox'
 import Home from '../home/Home'
 
 const Login = () => {
